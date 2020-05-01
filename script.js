34f921fc57b15c18f35a5025c20ab5da493a731d
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-05-01 12:31:31 -0700

    commit7

--- script.js
+++ script.js
@@ -60,3 +60,60 @@ function addTransactionDOM(transaction) {
 
   // Add class based on value
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = `
+  ${transaction.text} <span>${sign}${Math.abs(
+  transaction.amount
+)}</span> <button class="delete-btn" onclick="removeTransaction(${
+  transaction.id
+})">x</button>
+`;
+
+list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+const amounts = transactions.map(transaction => transaction.amount);
+
+const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+const income = amounts
+  .filter(item => item > 0)
+  .reduce((acc, item) => (acc += item), 0)
+  .toFixed(2);
+
+const expense = (
+  amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+  -1
+).toFixed(2);
+
+balance.innerText = `$${total}`;
+money_plus.innerText = `$${income}`;
+money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+transactions = transactions.filter(transaction => transaction.id !== id);
+
+updateLocalStorage();
+
+init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+function init() {
+list.innerHTML = '';
+
+transactions.forEach(addTransactionDOM);
+updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
