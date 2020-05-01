edef7fb299254b695fc285bd4697ffe58ba2dbfe
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-05-01 09:26:42 -0700

    commit6

---
+++ script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const balance = document.getElementById('balan')
+const balance = document.getElementById('balance')
+const balance = document.getElementById('balance')
+const balance = document.getElementById('balance')
+const balance = document.getElementById('balance')
